a6563ecc891eeb0c2ed669503f3216507dcd6118
Benjamin Andrew <bandrew01@qub.ac.uk>
2020-05-17 22:38:50 +0100

    Support JSON requests

--- app.py
+++ app.py
@@ -7,8 +7,15 @@ DEFAULT_SENTIMENT_THRESHOLD = 0.1
 
 @app.route("/", methods=["POST"])
 def index():
-    text = request.form.get("text")
-    sentiment_threshold = float(request.form.get("sentiment_threshold", DEFAULT_SENTIMENT_THRESHOLD))
+    if request.form:
+        # Multipart Form
+        data = request.form
+    else:
+        # JSON
+        data = request.get_json()
+
+    text = data.get("text")
+    sentiment_threshold = float(data.get("sentiment_threshold", DEFAULT_SENTIMENT_THRESHOLD))
     return jsonify({"words": getSentiment(text, sentiment_threshold)})
 
 
